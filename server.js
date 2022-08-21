7f107cb08d99d34b981afb4d5233e7086892a55a
martin <martin@comets.kr>
2022-08-22 03:05:52 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const express = require("express");
 const res = require("express/lib/response");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
 // 1: Kirish code
 app.use(express.static("public"));
@@ -17,6 +27,10 @@ app.set("view engine", "ejs");
 // 4 Routing code
 app.post("/create-item", (req, res) => {
   // TODO: code with db here
+});
+
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
 });
 
 app.get("/", function (req, res) {
