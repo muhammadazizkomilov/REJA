08b0d3b0b0572742da28866d13d37f93075c7acc
muhammadazizkomilov <komilov.mukhammadaziz@gmail.com>
2023-10-01 04:32:02 +0900

    feat: CRUD operatsiyalari

--- app.js
+++ app.js
@@ -32,6 +32,17 @@ app.set("view engine", "ejs");
 
 // 4 Routing code
 app.post("/create-item", (req, res) => {
+  console.log("user entered /create-item");
+  console.log(req.body);
+  const new_reja =req.body.reja;
+  db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+  if (err) {
+    console.log(err);
+    res.end("something went wrong");
+  } else {
+    res.end("succeessfully addend");
+  }
+});
   // TODO: code with db here
 });
 
@@ -40,7 +51,17 @@ app.get("/author", (req, res) => {
 });
 
 app.get("/", function (req, res) {
-  res.render("reja");
+  console.log("user entered /");
+  db.collection("plans")
+  .find()
+  .toArray((err, data) => {
+   if(err) {
+    console.log(err);
+    res.end("something went wrong");
+   } else {
+     res.render("reja", { items: data });
+   }
+  });
 });
 
 module.exports = app;
