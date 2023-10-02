51cb7be92d525a360030ad6695782afa14bfd349
muhammadazizkomilov <komilov.mukhammadaziz@gmail.com>
2023-10-03 06:50:14 +0900

    FIX:Reja frontent quramiz

--- app.js
+++ app.js
@@ -1,64 +1,74 @@
-console.log("Web Serverni boshlash");
+console.log("Web serverni boshlash");
 const express = require("express");
-const res = require("express/lib/response");
+const res = require("express/lib/response")
 const app = express();
-
+//const http = require("http");
 const fs = require("fs");
-
-
-// MongoDB connect chaqirish
+// mongono chaqirish;
 const db = require("./server").db();
+// bu db orqali databasega turli xilmalumotlarni yozish uqish uchurish yoki delete qilishni.
+//amalga oshiradi
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-  if (err) {
-    console.log("ERROR:", err);
-  } else {
-    user=JSON.parse(data);
-  }
-});
+// let user;
+// fs.readFile("database/user.json","utf8", (err,data) => {
+//     if(err) {
+//         console.log("ERROR:",err);
+// } else {
+//     user = JSON.parse(data);
+// }
+// })
 
-
-// 1: Kirish code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// MongoDB connect
 
 
-// 2: Session code
-// 3: Views code
-app.set("views", "views");
-app.set("view engine", "ejs");
+//-------------------1------------------//
+ app.use(express.static("public"));
+ app.use(express.json());
+app.use(express.urlencoded({extended: true }));
 
-// 4 Routing code
+//------------------2--------------//
+
+
+
+//-------------------------3-------------//
+app.set("views","views");
+app.set("view engine","ejs");
+
+//--------------------------4-----------------//
 app.post("/create-item", (req, res) => {
-  console.log("user entered /create-item");
-  // console.log(req.body.reja);
-  const new_reja = req.body.reja;
-  db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
-   console.log(data.ops);
-   res.json(ops [0]);
-});
-  
+   // console.log("user entered /create-item");
+   console.log(req.body);
+   const new_reja = req.body.reja;
+   db.collection("plans").insertOne({reja: new_reja}, (err,data) => {
+    console.log(data.ops);
+    res.json(data.ops[0]);
+    // if(err) {
+    //     console.log(err);
+    //     res.end("something went wrong")
+    // } else {
+    //     res.end("successfully added");
+    // }
+   });
+   //res.end("success") ;                                  // bu yerda boddyni tekshiri
+ //res.json({ test: "success"});
 });
 
-app.get("/author", (req, res) => {
-  res.render("author", { user:json });
-});
 
-app.get("/", function (req, res) {
-  console.log("user entered /");
-  db.collection("plans")
-  .find()
-  .toArray((err, data) => {
-   if(err) {
-    console.log(err);
-    res.end("something went wrong");
-   } else {
-     res.render("reja", { items: data });
-   }
+     app.get("/",function (req, res) {
+      console.log("user entered /create-item");
+      db.collection("plans")
+      .find()
+      .toArray((err, data) => {
+       if (err) {
+        console.log(err);
+        res.end("something went wrong");
+       } else {
+        //console.log(data);
+         res.render("reja",{items: data});
+    }
+   });
   });
-});
 
-module.exports = app;
+    module.exports = app;
 
+    
--- server.js
+++ server.js
@@ -1,29 +1,26 @@
 const http = require("http");
 const mongodb = require("mongodb");
 
-
 let db;
 const connectionString =
-"mongodb+srv://danny:CdWzaqRFEtB8R9Et@cluster0.ar5nuyp.mongodb.net/?retryWrites=true&w=majority";
-
+  "mongodb+srv://danny:CdWzaqRFEtB8R9Et@cluster0.ar5nuyp.mongodb.net/?retryWrites=true&w=majority";
 
 mongodb.connect(connectionString, {
- useNewUrlParser: true, 
- useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 },
- (err,client) => {
-    if(err) console.log("ERROR on connection MangoDB");
-    else{
-        console.log("MongoDB connection succeed");
-        module.exports = client;
+(err, client) => {
+  if (err) {
+    console.log("MongoDB ulanishda XATOLIK yuz berdi");
+  } else {
+    console.log("MongoDB ulanish muvaffaqiyatli amalga oshirildi");
+    module.exports = client;
 
-        const app = require("./app");
-        const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-  console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
+    const app = require("./app");
+    const server = http.createServer(app);
+    let PORT = 3000;
+    server.listen(PORT, function () {
+      console.log(`Server muvaffaqiyatli ishlayapti, PORT: ${PORT}, http://localhost:${PORT}`);
+    });
+  }
 });
-    }
-
-});
-
